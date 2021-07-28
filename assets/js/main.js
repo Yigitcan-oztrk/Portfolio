a5db0aa13120adcac2f7f89942eac97cb9dd903b
yiğitcan Öztürk <yozturk037@gmail.com>
2021-07-28 14:38:30 +0300

    portfolio is complate

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,4 +98,17 @@ modalCloses.forEach((modalClose) => {
   })
 })
 
+// =========================== PORTFOLİO SWİPER ===========================
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop:true,
 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true
+  },
+});
